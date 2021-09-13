5a9160ac0a0dcb5a552985aa433af5800b7c7232
Katherine Michel <kthrnmichel@gmail.com>
2021-09-12 21:24:27 -0500

    Add artwork test and format wit black

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
+
 from .models import Artwork, Image
+
 
 class ImageInline(admin.TabularInline):
     model = Image
 
+
 @admin.register(Artwork)
 class ArtworkAdmin(admin.ModelAdmin):
-    inlines = [
-        ImageInline
-    ]
+    inlines = [ImageInline]
--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from model_bakery import baker
 
-# Create your tests here.
+from .models import Artwork, Image
+
+
+class ModelTestCase(TestCase):
+    """A class that tests each model"""
+
+    def test_artwork_model(self):
+        artwork = baker.make(Artwork)
+        assert artwork.title
