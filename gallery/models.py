1fd4750d8f8a62778e10962a09fa7f84f829287e
Katherine Michel <kthrnmichel@gmail.com>
2021-09-12 21:12:56 -0500

    Add models

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Artwork(models.Model):
+    title = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.title
+
+
+def upload_gallery_image(self, filename):
+    return f"artworks/{filename}"
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to=upload_gallery_image)
+    artwork = models.ForeignKey(
+        Artwork, on_delete=models.CASCADE, related_name="images"
+    )
