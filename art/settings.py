e6b57b2d29e12cb68fd58038b81833b6c18969d1
Katherine Michel <kthrnmichel@gmail.com>
2021-09-13 23:16:27 -0500

    Fix secret key

--- art/settings.py
+++ art/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "django-insecure--e9r=rr^ob@i(tree%uysk=ghu@odhunhq+6f!j*%&@$y1%mj-"
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "django-insecure--e9r=rr^ob@i(tree%uysk=ghu@odhunhq+6f!j*%&@$y1%mj-"
+# SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
